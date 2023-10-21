b3819a6fd83f7a96bc092b5e349a1bc5684f6bc5
WildBarreto <wildsbsrreto@gmail.com>
2023-10-21 14:16:40 -0300

    Card component

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css'],
+})
+export class CardPricingComponent {
+  @Input()
+  gameType: string = 'Digital PS5';
+  @Input()
+  gamePrice: string = 'R$ 399,90';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
