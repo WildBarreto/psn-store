0ebf7bd649de33588258f981334e0820e364a1a7
WildBarreto <wildsbsrreto@gmail.com>
2023-10-21 14:40:40 -0300

    repassando paramentros de components

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
+  @Input()
+  gameCover: string = '';
+  @Input()
+  gameLabel: string = '';
+  constructor() {}
+  @Input()
+  gameType: string = 'Digital | PS5';
+  @Input()
+  gamePrice: string = 'R$ 399,90';
 
+  ngOnInit(): void {}
 }
